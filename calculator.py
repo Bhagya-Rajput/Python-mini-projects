abf4dc12d2ba7d149b7fad1fa06ad3b3f05275ba
Bhagya-Rajput <bhagyakumarrajput@gmail.com>
2026-06-27 17:49:41 +0530

    few changes

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 condition = True
 Result = 0
 continued = False
-try :
-    while condition :   
+while condition :   
+    try:
         if continued:
           a = Result
           b = int(input("enter new value"))
@@ -10,9 +10,6 @@ try :
         else:
            a,b  = map(int, input("Enter numbers  separated by space: ").split(" "))
         operator = input(" enter the operation you want to do")
-        
-           
-           
         match operator:
             case "+":
                 Result = a+b
@@ -30,23 +27,25 @@ try :
                 Result = a**b 
             case _:
                 print("entered invalid operator")
+                continue
         if isinstance(Result,float):
           print(f"your result is:{Result:.2f}")
         else :
           print(f"your result is:{Result}")
 
-        retaken = input("if you want to continue enter { Yes -> Y}/{ New operation ->C}/{ Quit }").capitalize()
+        retaken = input("if you want to continue enter { Yes -> Y}/{ New operation ->C}/{ Quit }").strip().upper()
         if(retaken == "Y"):
-         condition = continued = (retaken == "Y")
+         continued = retaken == "Y"
         elif(retaken == "C"): 
          Result = 0
+         continued = False
         else:
          break
+    except ValueError as e:
+        print("You enterd wrong Input",e)
+    except ZeroDivisionError:
+        print("You are trying to divide by zero")
 
 
-except ValueError as e:
-    print("You enterd wrong Input",e)
-except ZeroDivisionError:
-    print("You are trying to divide by zero")
 
 
