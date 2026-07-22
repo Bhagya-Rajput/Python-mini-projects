35c29d3b5d168da9fdfab2091976cb29d83c4bd3
Bhagya-Rajput <bhagyakumarrajput@gmail.com>
2026-07-22 15:01:08 +0530

    board none printing is fixed

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -3,6 +3,5 @@ def board():
    for i in range(3):
       for j,k in zip(range(3),lst[i*3:(i+1)*3]):
          print(f" {k} ", end = "|"if j<2  else '\n')
-      print("----------" if i<2 else print())
-
+      print("----------" if i<2 else "")
 board()
